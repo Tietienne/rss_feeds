b9404333f0ca6c3964665355db879624844b42c6
Etienne ALEXANDRE <tietienne77@gmail.com>
2022-11-08 00:14:52 +0100

    fix: primary key

--- src/main/java/bdd/bigdata/rss_feeds/tables/Userid_by_link.java
+++ src/main/java/bdd/bigdata/rss_feeds/tables/Userid_by_link.java
@@ -1,16 +1,18 @@
 package bdd.bigdata.rss_feeds.tables;
 
+import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
 import org.springframework.data.cassandra.core.mapping.Column;
 import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
 import org.springframework.data.cassandra.core.mapping.Table;
 
 import java.util.Objects;
 
 @Table
 public class Userid_by_link {
-    @PrimaryKey
+    @PrimaryKeyColumn(ordinal = 0, type = PrimaryKeyType.PARTITIONED)
     private final String link;
-    @Column
+    @PrimaryKeyColumn(ordinal = 0, type = PrimaryKeyType.CLUSTERED)
     private final String userId;
 
     public Userid_by_link(String link, String userId) {
