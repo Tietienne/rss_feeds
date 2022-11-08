d5f43ca234bb78284dc5b942c420caebc7bed433
Etienne ALEXANDRE <tietienne77@gmail.com>
2022-11-08 21:12:47 +0100

    doc: add doc to controller

--- src/main/java/bdd/bigdata/rss_feeds/CassandraController.java
+++ src/main/java/bdd/bigdata/rss_feeds/CassandraController.java
@@ -13,7 +13,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-@CrossOrigin(origins = "http://localhost:8081")
 @RestController
 public class CassandraController {
     @Autowired
